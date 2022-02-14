f60b6581dd811e3e436f8a877e7d069a3e37b37a
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-02-14 15:07:56 +0200

    Revert "medra version"
    This reverts commit 0cad86dedefc6f84a864462bc1a6ce46a10fbcc4.

--- esr21_subject/models/special_interest_adverse_event.py
+++ esr21_subject/models/special_interest_adverse_event.py
@@ -50,6 +50,7 @@ class SpecialInterestAdverseEventRecord(SiteModelMixin, BaseUuidModel):
     meddra_version = models.CharField(
         verbose_name='MedDRA version',
         max_length=5,
+
     )
 
     start_date = models.DateField(
